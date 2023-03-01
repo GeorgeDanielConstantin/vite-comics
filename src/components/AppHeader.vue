3a5832aa373b4b12894e984325d939f7ca6be0ee
GeorgeDanielConstantin <danielgconstantin@gmail.com>
2023-03-01 17:32:11 +0100

    add header add jumbo add cards

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,49 @@
+<script></script>
+
+<template>
+  <section>
+    <header class="container">
+      <img src="../assets/img/dc-logo.png" alt="Logo" />
+      <ul class="d-flex">
+        <li><a href="#">CHARACTERS</a></li>
+        <li><a href="#">COMICS</a></li>
+        <li><a href="#">MOVIES</a></li>
+        <li><a href="#">TV</a></li>
+        <li><a href="#">GAMES</a></li>
+        <li><a href="#">COLLECTIBLES</a></li>
+        <li><a href="#">VIDEOS</a></li>
+        <li><a href="#">FANS</a></li>
+        <li><a href="#">NEWS</a></li>
+        <li><a href="#">SHOP</a></li>
+      </ul>
+    </header>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  background-color: white;
+  padding: 2rem;
+}
+header {
+  display: flex;
+  justify-content: space-between;
+}
+
+ul {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  li {
+    a {
+      color: black;
+      text-decoration: none;
+      padding: 2rem 1rem;
+      &:hover {
+        color: blue;
+        border-bottom: 4px solid blue;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,15 @@
+<script></script>
+
+<template>
+  <section></section>
+</template>
+
+<style lang="scss" scoped>
+section {
+  background-image: url(../assets/img/jumbotron.jpg);
+  min-height: 50vh;
+  background-repeat: no-repeat;
+  background-position: top;
+  background-size: cover;
+}
+</style>
